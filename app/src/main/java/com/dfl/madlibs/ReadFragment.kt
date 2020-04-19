dba58c683566d0cb8df49d4cad418de69176847f
dellylucas <dellylucas@hotmail.com>
2020-04-19 00:06:39 -0500

    scroll

--- app/src/main/java/com/dfl/madlibs/ReadFragment.kt
+++ app/src/main/java/com/dfl/madlibs/ReadFragment.kt
@@ -1,12 +1,14 @@
 package com.dfl.madlibs
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_read.*
+
 
 class ReadFragment : Fragment() {
 
@@ -20,6 +22,7 @@ class ReadFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         storyTextView.text = arguments?.getString("story")
+        storyTextView.movementMethod = ScrollingMovementMethod()
         makeAnotherButton.setOnClickListener {
             val action = ReadFragmentDirections.actionReadFragmentToStoryFragment()
             view.findNavController().navigate(action)
